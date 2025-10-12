efee7ad4a16c189ea515e81b30c4b48903d66d3c
Inurria979 <j.inurria@gmail.com>
2025-10-12 02:28:14 +0200

    Passed 496/554

--- intermediate/magician/sol.rs
+++ intermediate/magician/sol.rs
@@ -9,7 +9,7 @@ Each spell can be cast only once.
 
 Return the maximum possible total damage that a magician can cast.
 
- 
+
 
 Example 1:
 
@@ -31,7 +31,7 @@ Explanation:
 
 The maximum possible damage of 13 is produced by casting spells 1, 2, 3 with damage 1, 6, 6.
 
- 
+
 
 Constraints:
 
@@ -41,55 +41,72 @@ Constraints:
 
 use std::collections::HashMap;
 
-fn main(){
-    let power1 = Vec::<i32>::from([1,1,3,4]);
-    let power2 = Vec::<i32>::from([7,1,6,6]);
+fn main() {
+    let power1 = Vec::<i32>::from([1, 1, 3, 4]);
+    let power2 = Vec::<i32>::from([7, 1, 6, 6]);
+    let power3 = Vec::<i32>::from([3, 4, 7, 3, 3, 1, 1]);
 
+    //In this case is falling because the best option is build using two set with smaller vals
+    //Expected result [6, 6, 3, 3] = 18
+    //Result [5, 5, 5] = 15
+    let power4 = Vec::<i32>::from([4, 5, 5, 6, 3, 6, 5, 3, 4]);
     print!("\nThe max val is: {:?}\n", maximum_total_damage(power1));
     print!("\nThe max val is: {:?}\n", maximum_total_damage(power2));
+    print!("\nThe max val is: {:?}\n", maximum_total_damage(power3));
+    print!("\nThe max val is: {:?}\n", maximum_total_damage(power4));
 }
 pub fn maximum_total_damage(power: Vec<i32>) -> i64 {
-
     let val_ocu = create_pair_power_occurences(power.clone());
     let mut set_of_keys = Vec::<i32>::new();
     //while some key exist it is possible to find an usable power
     let mut mut_val_ocu = val_ocu.clone();
-    while mut_val_ocu.len() > 0{
+    while mut_val_ocu.len() > 0 {
         let key = get_best_option(mut_val_ocu.clone());
-        if set_of_keys.len() == 0{
+        if set_of_keys.len() == 0 {
             set_of_keys.push(key);
-        }else if (set_of_keys.get(set_of_keys.len()-1).unwrap() - key).abs() > 2 {
-            set_of_keys.push(key)
+        } else {
+            let mut flag: bool = true;
+            for i in &set_of_keys.clone() {
+                //the condition must be true for all vals
+                if (i - key).abs() <= 2 {
+                    flag = false;
+                }
+            }
+            if flag {
+                set_of_keys.push(key);
+            }
         }
         //Remove this key
         mut_val_ocu.remove(&key);
     }
     let mut sum: i64 = 0;
-    for key in &set_of_keys{
+    for key in &set_of_keys {
         sum += i64::from(*val_ocu.get(key).unwrap()) * i64::from(*key);
-        //print!("\nKey: {:?} VAL: {:?}", key, *val_ocu.get(key).unwrap());
+        print!("\nKey: {:?} VAL: {:?}", key, *val_ocu.get(key).unwrap());
     }
-    
+
     return sum;
 }
-fn create_pair_power_occurences(v: Vec<i32>)-> HashMap<i32, i32>{
+fn create_pair_power_occurences(v: Vec<i32>) -> HashMap<i32, i32> {
     let mut pair = HashMap::<i32, i32>::new();
-    for i in &v{
-        if  !pair.contains_key(i) {
+    for i in &v {
+        if !pair.contains_key(i) {
             pair.insert(*i, 1);
-        }else {
-            pair.insert(*i, pair.get(i).unwrap()+1);
+        } else {
+            pair.insert(*i, pair.get(i).unwrap() + 1);
         }
     }
     return pair;
 }
-fn get_best_option(val_ocu: HashMap<i32, i32>) -> i32{
-    
+
+//I need to add a check which verify that there is not exist a combination
+//of sub_max pairs better than the best pair
+fn get_best_option(val_ocu: HashMap<i32, i32>) -> i32 {
     let mut best_key: i32 = 0;
     let mut best_mult: i32 = 0;
-    for (key, val) in val_ocu{
-        if key*val > best_mult {
-            best_mult = key*val;
+    for (key, val) in val_ocu {
+        if key * val > best_mult {
+            best_mult = key * val;
             best_key = key;
         }
     }
